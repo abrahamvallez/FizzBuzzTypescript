43d0a10d64a067077e50865f0e3315c52022c9fd
Abraham Vallez <abevallez@gmail.com>
2022-03-20 23:04:32 +0100

    refactor tests

--- src/FizzBuzz.test.ts
+++ src/FizzBuzz.test.ts
@@ -1,15 +1,9 @@
 import { fizzBuzz } from './FizzBuzz'
 
 describe('FizzBuzz should', () => {
-  it('should return 1 when input is 1', () => {
-    expect(fizzBuzz(1)).toBe('1')
-  })
-
-  it('should return 2 when input is 2', () => {
-    expect(fizzBuzz(2)).toBe('2')
-  })
-
-  it('should return 4 when input is 4', () => {
-    expect(fizzBuzz(4)).toBe('4')
+  
+  it.each([[1,'1'], [2,'2'], [4,'4']])
+  ('should return the string representation of the input number', (inputNumber: number, stringOutput: string) => {
+    expect(fizzBuzz(inputNumber)).toBe(stringOutput)
   })
 })
